5d07cbfad926cc15c84523b88d2304b2dfcd6636
isobek Vafoyev <isobekvafoyev9@gmail.com>
2025-09-12 18:32:31 +0500

    start

---
+++ src/main/java/com/istadium/istadium_api/IstadiumApiApplication.java
@@ -0,0 +1,13 @@
+package com.istadium.istadium_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstadiumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IstadiumApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/istadium/istadium_api/IstadiumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.istadium.istadium_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IstadiumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
